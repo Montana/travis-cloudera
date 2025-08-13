7dc40ad2e239092b4022a1cd81463f35f6925113
Michael Mendy <montana@linux.com>
2025-08-13 03:18:51 -0700

    rm -rf

--- scripts/setup_system.sh
+++ scripts/setup_system.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 set -e
 
-echo "=== Setting up system packages ==="
-sudo rm -f /etc/apt/sources.list.d/pgdg.list
-sudo apt-get update -qq --allow-releaseinfo-change || sudo apt-get update -qq
-sudo apt-get install -y openjdk-8-jdk
-sudo update-alternatives --set java /usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java
-sudo apt-get install -y wget curl
-java -version
+wget -q https://archive.cloudera.com/cm7/${CLOUDERA_MANAGER_VERSION}/ubuntu2004/apt/cloudera-manager.list
+
+sudo cp cloudera-manager.list /etc/apt/sources.list.d/
+
+wget -q https://archive.cloudera.com/cm7/${CLOUDERA_MANAGER_VERSION}/ubuntu2004/apt/archive.key
+sudo apt-key add archive.key
+
+sudo apt-get update
+
+sudo apt-get install -y cloudera-manager-agent
+
+sudo mkdir -p /opt/cloudera/parcels
+sudo mkdir -p /etc/hadoop/conf
+sudo mkdir -p /var/log/cloudera-scm-agent
+
+sudo chown -R $USER:$USER /opt/cloudera
+sudo chown -R $USER:$USER /var/log/cloudera-scm-agent
+
+rm -f cloudera-manager.list archive.key
